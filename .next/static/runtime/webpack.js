1f4ef538e325a0169912a2f92f02d9a91ec1be6e
Sonya <sonjay08@gmail.com>
2019-01-13 14:13:40 -0600

    imported items componenet into index.js

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1a0eb6a9320360b9fe17";
+/******/ 	var hotCurrentHash = "bf642adb723eb8fdf5d8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/Items.js
+++ components/Items.js
@@ -1,10 +1,11 @@
-import React, { Componenet } from 'react';
+import React, { Component } from 'react';
+import Query from 'react-apollo';
 
 export default class Items extends Component {
     render() {
         return (
             <div>
-                
+                <p>Items</p>
             </div>
         )
     }
